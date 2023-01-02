130ca58c7031f7582c6a09ebba373028958eec28
Anmol Jhamb <talktoanmol@outlook.com>
2023-01-02 22:48:33 +0530

    added required:true for product Schema

--- api/models/products.js
+++ api/models/products.js
@@ -2,9 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const productSchema = Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    price: Number,
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
 });
 
 const Product = mongoose.model("product", productSchema);
