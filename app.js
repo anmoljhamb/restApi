88fd14841adc91cb5e23d17e11344625e1074828
Anmol Jhamb <talktoanmol@outlook.com>
2023-01-02 20:25:53 +0530

    added morgan for logging

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const productRouter = require("./api/routes/products");
 const orderRouter = require("./api/routes/orders");
 
+app.use(morgan("dev"));
 app.use("/products", productRouter);
 app.use("/orders", orderRouter);
 
