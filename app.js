8bd71d3bae2c7f2166ed0baa79a3e3f61156c551
Anmol Jhamb <talktoanmol@outlook.com>
2023-01-02 20:29:45 +0530

    error handling for unmatched route, and for future generated errors

--- app.js
+++ app.js
@@ -8,4 +8,19 @@ app.use(morgan("dev"));
 app.use("/products", productRouter);
 app.use("/orders", orderRouter);
 
+app.use((req, res, next) => {
+    // handling for all the unmatched routes.
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message,
+        },
+    });
+});
+
 module.exports = app;
