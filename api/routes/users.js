062af17c707f3ba82dbabd765077a5d088b9f289
Anmol Jhamb <talktoanmol@outlook.com>
2023-01-03 17:42:29 +0530

    renamed models/user to models/users

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const router = express.Router();
-const User = require("../models/user");
+const User = require("../models/users");
 
 router.get("/", (req, res) => {
     User.find()
